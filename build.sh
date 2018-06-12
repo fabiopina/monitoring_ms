bd1c2ef252e205d12c92baf0486bc070b26fd282
fabiopina <fpina@student.dei.uc.pt>
2018-06-12 14:55:52 +0100

    updated build.sh and docker-compose

--- build.sh
+++ build.sh
@@ -1,102 +1,23 @@
 #!/bin/bash
 
-# Delete containers
-docker rm microservices_aggr_1
-docker rm microservices_playlists_1
-docker rm microservices_auth_1
-docker rm microservices_users_1
-docker rm microservices_songs_1
-docker rm microservices_zuul_1
-docker rm microservices_eureka_1
-docker rm microservices_log-server_1
-
 # Delete images
-docker rmi users-ms
-docker rmi fabiopina151/microservices:users-ms
-docker rmi songs-ms
-docker rmi fabiopina151/microservices:songs-ms
-docker rmi playlists-ms
-docker rmi fabiopina151/microservices:playlists-ms
-docker rmi aggr-ms
-docker rmi fabiopina151/microservices:aggr-ms
-docker rmi auth-ms
-docker rmi fabiopina151/microservices:auth-ms
-docker rmi eureka-server
-docker rmi fabiopina151/microservices:eureka-server
-docker rmi zuul-gateway
-docker rmi fabiopina151/microservices:zuul-gateway
-docker rmi log-server
-docker rmi fabiopina151/microservices:log-server
+docker rmi fabiopina151/eureka-server:0.1
+docker rmi fabiopina151/zuul-gateway:0.1
 
 # Zuul
 cd Zuul-Gateway
 mvn clean
 mvn install
-docker build -t zuul-gateway .
+docker build -t fabiopina151/zuul-gateway:0.1 .
 cd ..
 
 # Eureka
 cd Eureka-Server
 mvn clean
 mvn install
-docker build -t eureka-server .
-cd ..
-
-# Aggregator
-cd Aggregator_MS
-docker build -t aggr-ms .
-cd ..
-
-# Authentication
-cd Authentication_MS
-docker build -t auth-ms .
-cd ..
-
-# Playlists
-cd Playlists_MS
-docker build -t playlists-ms .
-cd ..
-
-# Songs
-cd Songs_MS
-docker build -t songs-ms .
-cd ..
-
-# Users
-cd Users_MS
-docker build -t users-ms .
-cd ..
-
-# Log-Server
-cd Log_Server
-docker build -t log-server .
+docker build -t fabiopina151/eureka-server:0.1 .
 cd ..
 
 # Upload images to docker hub
-docker tag zuul-gateway:latest fabiopina151/microservices:zuul-gateway
-docker push fabiopina151/microservices:zuul-gateway
-
-docker tag eureka-server:latest fabiopina151/microservices:eureka-server
-docker push fabiopina151/microservices:eureka-server
-
-docker tag aggr-ms:latest fabiopina151/microservices:aggr-ms
-docker push fabiopina151/microservices:aggr-ms
-
-docker tag auth-ms:latest fabiopina151/microservices:auth-ms
-docker push fabiopina151/microservices:auth-ms
-
-docker tag playlists-ms:latest fabiopina151/microservices:playlists-ms
-docker push fabiopina151/microservices:playlists-ms
-
-docker tag songs-ms:latest fabiopina151/microservices:songs-ms
-docker push fabiopina151/microservices:songs-ms
-
-docker tag users-ms:latest fabiopina151/microservices:users-ms
-docker push fabiopina151/microservices:users-ms
-
-docker tag log-server:latest fabiopina151/microservices:log-server
-docker push fabiopina151/microservices:log-server
-
-
-
-
+docker push fabiopina151/zuul-gateway:0.1
+docker push fabiopina151/eureka-server:0.1
