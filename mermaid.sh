f2c53792c69d88f5cfdb811cffe0727722f81b08
fabiopina <fpina@student.dei.uc.pt>
2018-07-14 21:39:30 +0100

    cleaned repository

--- mermaid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rmi fabiopina151/mermaid-syntax-server:0.1
-cd Mermaid-Syntax-Server
-docker build -t fabiopina151/mermaid-syntax-server:0.1 . 
-docker push fabiopina151/mermaid-syntax-server:0.1
